4bcc7def86962af97d086a7a0434c1cfe44ae63b
kodah <lord@lords-iMac.home>
2017-01-10 13:40:37 +0000

    Remove /Pods from repo

--- Pods/Target Support Files/Pods-BodyTrack/Pods-BodyTrack-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BodyTrack : NSObject
-@end
-@implementation PodsDummy_Pods_BodyTrack
-@end
--- Pods/Target Support Files/Pods-BodyTrackTests/Pods-BodyTrackTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BodyTrackTests : NSObject
-@end
-@implementation PodsDummy_Pods_BodyTrackTests
-@end
